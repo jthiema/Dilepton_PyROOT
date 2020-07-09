255ad749b2e28f3ab81d485dcd8b22c9ff0d4b0c
Isaiah Jeffrey Ertel <iertel@hammer-fe01.rcac.purdue.edu>
2020-07-09 19:48:29 -0400

    debugging the branches

--- IsaiahDevLeptonOnly.py
+++ IsaiahDevLeptonOnly.py
@@ -36,26 +36,29 @@ tree.SetBranchAddress("lepPfIso",v_lepPfIso)
 v_lepSCEta = ROOT.std.vector('float')()
 tree.SetBranchAddress("lepSCEta",v_lepSCEta)
 
+
+### Isaiah dev
 #Vector of four vector for GenLepton
-v_GenLepton = ROOT.Math.LorentzVector('ROOT::Math::PtEtaPhiM4D<float>')()
-tree.SetBranchAddress("GenLepton",GenLepton)
+v_GenLeptons = ROOT.Math.LorentzVector('ROOT::Math::PtEtaPhiM4D<float>')()
+tree.SetBranchAddress("GenLepton",v_GenLeptons)
 
 #Vector of four vector for GenAntiLepton
 v_GenAntiLepton = ROOT.Math.LorentzVector('ROOT::Math::PtEtaPhiM4D<float>')()
-tree.SetBranchAddress("GenAntiLepton",GenAntiLepton)
+tree.SetBranchAddress("GenAntiLepton",v_GenAntiLepton)
 
 #Vector of four vector for GenLeptPdgId
-v_GenLepPdgId = ROOT.Math.LorentzVector('ROOT::Math::PtEtaPhiM4D<float>')()
-tree.SetBranchAddress("GenLeptonPdgId",v_GenLeptonPdgId)
+v_GenLepPdgId = ROOT.std.vector('int')()
+tree.SetBranchAddress("GenLepton",v_GenLepPdgId)
 
 #Vector of four vector for GenAntiLeptPdgId
-v_GenAntiLepPdgId = ROOT.Math.LorentzVector('ROOT::Math::PtEtaPhiM4D<float>')()
-tree.SetBranchAddress("GenAntiLepton",v_GenAntiLeptonPdgId)
+v_GenAntiLepPdgId = ROOT.std.vector('int')()
+tree.SetBranchAddress("GenAntiLepton",v_GenAntiLepPdgId)
+### End of Vectors that needed to be added
+
 
 nUnmatchedleptons = 0
 
-histpt = ROOT.TH2F("Pt for Leptons", "Pt for Leptons",
-     150,50.e3,200.e3)
+histpt = ROOT.TH2F("histpt", "Pt for Leptons", 60 , 0 , 1000 , 60 , 0 , 1000)
 
 for i in range(10000) :
 
@@ -63,12 +66,12 @@ for i in range(10000) :
     v_leptons.clear()
     v_lepPfIso.clear()
     v_lepSCEta.clear()
-    v_lepID_MuonTight.clear()
-    v_lepID_ElecCutBased.clear()
-    v_lepID_MuonTight.clear()
-    v_jets.clear()
-    v_jetPFID.clear()
-    v_jetBTagDeepCSV.clear()
+#    v_lepID_MuonTight.clear()
+#    v_lepID_ElecCutBased.clear()
+#    v_lepID_MuonTight.clear()
+#    v_jets.clear()
+#    v_jetPFID.clear()
+#    v_jetBTagDeepCSV.clear()
    
     # Very important; this is where the data gets filled from the tree
     tree.GetEntry(i)
@@ -82,20 +85,19 @@ for i in range(10000) :
     # Only consider events with invariant mass of lepton pair > 20
     if (v_leptons[0] + v_leptons[1]).M() < 20.0 : continue 
     # Only consider events with MET > 20
-    if (met.Pt() < 20.0) : continue
-        
-    if lepPdgId[0] == genlepPdgId[0] :
-        # Fill 
-        histpt.Fill(v_lepton.pt(),v_genlepton.pt())
-    if lepPdgId[0] == genlepPdgId[1] :
-        # Fill 
-        histpt.Fill(v_lepton.pt(),v_genlepton.pt())
-    else : 
-        nUnmatchedleptons += 1
-        
-        
-print('Unmatched Leptons',nUnmatchedleptons)
+#    if (met.Pt() < 20.0) : continue
     
+    print('Lepton: ',v_lepPdgId)
+    print('Gen Lepton: ',v_GenLepPdgId)
+    print('\n\n\n')
+    
+    histpt.Fill(v_lepPdgId[0],v_GenLepPdgId)
+    
+histpt.SetDirectory(0)
+file.Close()
+histpt.cd()
+histpt.Write()
+
     
     
     
