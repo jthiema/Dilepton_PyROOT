a1da6e966ba0864cbcb886a8418fed9e3e540b04
Hyeon Seo Yun <yun79@hammer-fe00.rcac.purdue.edu>
2020-08-05 17:30:44 -0400

    working to add normalization

--- test_train_seperate.py
+++ test_train_seperate.py
@@ -14,7 +14,12 @@ X_test = X[int(data_size*ratio):]
 Y_train = Y[:int(data_size*ratio)]
 Y_test = Y[int(data_size*ratio):]
 
-np.save("X_train.npy", X_train)
-np.save("X_test.npy", X_test)
-np.save("Y_train.npy", Y_train)
-np.save("Y_test.npy", Y_test)
+"""
+Now we do normalization. Eta values are normalized to be eta_normalized = (eta +2.5) /5
+phi values are normalized to be 
+"""
+
+np.save("X_train_normalized.npy", X_train)
+np.save("X_test_normalized.npy", X_test)
+np.save("Y_train_normalized.npy", Y_train)
+np.save("Y_test_normalized.npy", Y_test)
