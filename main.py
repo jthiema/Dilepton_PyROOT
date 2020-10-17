f9f6712a65c0d372f4e444ca42859660feb385f8
Hyeon Seo Yun <yun79@hammer-fe01.rcac.purdue.edu>
2020-10-16 21:04:32 -0400

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import numpy as np
+from tensorflow import keras
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM, Bidirectional, BatchNormalization, TimeDistributed
 import tensorflow as tf
@@ -22,7 +23,8 @@ NAME = "Vanilla_normalized_leaky"
 print(NAME)
 
 tensorboard = TensorBoard(log_dir = 'logs/{}'.format(NAME)) #'logs/{}''
-checkpoint_path = "checkpoints/{}/cp.ckpt".format(NAME)
+#checkpoint_path = "checkpoints/{}/cp.ckpt".format(NAME)
+checkpoint_path = "checkpoints/{}/cp.h5".format(NAME)
 w_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path, save_weights_only=True, monitor='val_loss', verbose=1) # monitor vallidation loss to save if it's best it's seen
 
 model = Sequential()
@@ -39,6 +41,15 @@ model.add(TimeDistributed(Dense(30, activation=tf.nn.leaky_relu)))
 model.add(TimeDistributed(Dense(3, activation='linear'))) # output layer
 
 model.compile(optimizer=tf.keras.optimizers.Adam(lr=1e-4), loss=tf.keras.losses.MeanSquaredError(), metrics = ["accuracy"])
-history = model.fit(X_train, Y_train, epochs= 22, validation_data= (X_test,Y_test), callbacks = [w_callback, tensorboard])
-
+#model.load_weights(checkpoint_path)
+#model=keras.models.load_model(checkpoint_path)
+history = model.fit(X_train, Y_train, epochs= 17, validation_data= (X_test,Y_test), callbacks = [ tensorboard])
+model.save(checkpoint_path)
+#print("History keys: ", history.history.keys())
+fig = plt.figure()
+plt.plot(history.history['loss'], label="overall training loss")
+plt.plot(history.history['val_loss'], label="overall validation loss")
+plt.title('Training vs Validation Losses')
+plt.legend(loc='upper left')
+fig.savefig('Training vs Validation Losses')
 
